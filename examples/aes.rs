b99a5bc89525903ff4f3970403898b76056b71d4
Tmplt <tmplt@dragons.rocks>
2019-08-10 19:57:11 +0200

    handle 16B chunks as base, impl <const N: usize> above that

--- examples/aes.rs
+++ examples/aes.rs
@@ -2,11 +2,12 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use s32k144;
 use eaesy_rtfm::{self, AES128Cbc};
+use s32k144;
+use s32k144evb::wdog;
 
-const MSG: &[u8] = b"Key:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789ab";
 const MSG_LEN: usize = 16 * 10;
+const MSG: [u8; MSG_LEN] = *b"Key:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789abKey:0123456789ab";
 const IV: [u8; 16] = [0; 16];
 const PLAINKEY: [u8; 16] = [
     0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c,
@@ -15,6 +16,14 @@ const PLAINKEY: [u8; 16] = [
 #[entry]
 fn main() -> ! {
     let p = s32k144::Peripherals::take().unwrap();
+
+    // Disable watchdog
+    let wdog_settings = wdog::WatchdogSettings {
+        enable: false,
+        ..Default::default()
+    };
+    let _wdog = wdog::Watchdog::init(&p.WDOG, wdog_settings).unwrap();
+
     let mut aes = eaesy_rtfm::s32k144AES::new(p.FTFC, p.CSE_PRAM);
 
     let mut enctext: [u8; MSG_LEN] = [0; MSG_LEN];
--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,9 @@
 //! ```
 #![no_std]
 #![allow(non_camel_case_types)]
+#![feature(const_generics)]
 
+use core::convert::TryInto;
 use s32k144;
 use s32k144evb::csec;
 use s32k144evb::csec::CommandResult as Error;
@@ -20,8 +22,35 @@ use s32k144evb::csec::CommandResult as Error;
 pub trait AES128Cbc {
     type Error;
 
-    fn encrypt(&mut self, key: [u8; 16], iv: [u8; 16], plaintext: &[u8], cipher: &mut [u8]) -> Result<(), Self::Error>;
-    fn decrypt(&mut self, key: [u8; 16], iv: [u8; 16], cipher: &[u8], plaintext: &mut [u8]) -> Result<(), Self::Error>;
+    fn impl_encrypt(
+        &mut self,
+        key: [u8; 16],
+        iv: [u8; 16],
+        plaintext: &[u8; 16],
+        cipher: &mut [u8; 16],
+    ) -> Result<(), Self::Error>;
+    fn encrypt<const N: usize>(
+        &mut self,
+        key: [u8; 16],
+        iv: [u8; 16],
+        plaintext: &[u8; N],
+        cipher: &mut [u8; N],
+    ) -> Result<(), Self::Error>;
+
+    fn impl_decrypt(
+        &mut self,
+        key: [u8; 16],
+        iv: [u8; 16],
+        cipher: &[u8; 16],
+        plaintext: &mut [u8; 16],
+    ) -> Result<(), Self::Error>;
+    fn decrypt<const N: usize>(
+        &mut self,
+        key: [u8; 16],
+        iv: [u8; 16],
+        cipher: &[u8; N],
+        plaintext: &mut [u8; N],
+    ) -> Result<(), Self::Error>;
 
     // TODO: default trait implementation is software implementation
     // Notify user if SW-impl is used
@@ -33,20 +62,80 @@ pub struct s32k144AES {
 
 impl s32k144AES {
     pub fn new(ftfc: s32k144::FTFC, cse_pram: s32k144::CSE_PRAM) -> Self {
-        s32k144AES { csec: csec::CSEc::init(ftfc, cse_pram), }
+        s32k144AES {
+            csec: csec::CSEc::init(ftfc, cse_pram),
+        }
     }
 }
 
 impl AES128Cbc for s32k144AES {
     type Error = Error;
 
-    fn encrypt(&mut self, key: [u8; 16], iv: [u8; 16], plaintext: &[u8], cipher: &mut [u8]) -> Result<(), Error> {
+    fn impl_encrypt(
+        &mut self,
+        key: [u8; 16],
+        iv: [u8; 16],
+        plaintext: &[u8; 16],
+        cipher: &mut [u8; 16],
+    ) -> Result<(), Error> {
         self.csec.load_plainkey(&key)?;
         self.csec.encrypt_cbc(plaintext, &iv, cipher)
     }
 
-    fn decrypt(&mut self, key: [u8; 16], iv: [u8; 16], cipher: &[u8], plaintext: &mut [u8]) -> Result<(), Error> {
+    fn encrypt<const N: usize>(
+        &mut self,
+        key: [u8; 16],
+        iv: [u8; 16],
+        plaintext: &[u8; N],
+        cipher: &mut [u8; N],
+    ) -> Result<(), Error> {
+        if N % 16 != 0 {
+            return Err(Error::GeneralError);
+        }
+
+        // NOTE(unwrap): With the check above, each chunk will always have the length 16.
+        for (plaintext_chunk, cipher_chunk) in plaintext.chunks(16).zip(cipher.chunks_mut(16)) {
+            self.impl_encrypt(
+                key,
+                iv,
+                plaintext_chunk.try_into().unwrap(),
+                cipher_chunk.try_into().unwrap(),
+            )?;
+        }
+        Ok(())
+    }
+
+    fn impl_decrypt(
+        &mut self,
+        key: [u8; 16],
+        iv: [u8; 16],
+        cipher: &[u8; 16],
+        plaintext: &mut [u8; 16],
+    ) -> Result<(), Error> {
         self.csec.load_plainkey(&key)?;
         self.csec.decrypt_cbc(cipher, &iv, plaintext)
+    }
+
+    fn decrypt<const N: usize>(
+        &mut self,
+        key: [u8; 16],
+        iv: [u8; 16],
+        cipher: &[u8; N],
+        plaintext: &mut [u8; N],
+    ) -> Result<(), Error> {
+        if N % 16 != 0 {
+            return Err(Error::GeneralError);
+        }
+
+        // NOTE(unwrap): With the check above, each chunk will always have the length 16.
+        for (plaintext_chunk, cipher_chunk) in plaintext.chunks_mut(16).zip(cipher.chunks(16)) {
+            self.impl_decrypt(
+                key,
+                iv,
+                cipher_chunk.try_into().unwrap(),
+                plaintext_chunk.try_into().unwrap(),
+            )?;
+        }
+        Ok(())
     }
 }
