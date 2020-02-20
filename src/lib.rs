288792860fda18ef1fa22409cf32c524d7ff9e54
tmplt <tmplt@dragons.rocks>
2020-02-20 18:10:13 +0100

    refactor: move impl_* out of the trait

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,6 @@
 //! AES128Cbc::decrypt(...) // ?
 //! ```
 #![no_std]
-#![allow(non_camel_case_types)]
 
 use core::convert::TryInto;
 use s32k144;
@@ -21,33 +20,19 @@ use s32k144evb::csec::CommandResult as Error;
 pub trait AES128Cbc {
     type Error;
 
-    fn impl_encrypt(
-        &mut self,
-        key: [u8; 16],
-        iv: [u8; 16],
-        plaintext: &[u8; 16],
-        cipher: &mut [u8; 16],
-    ) -> Result<(), Self::Error>;
     fn encrypt(
         &mut self,
         key: [u8; 16],
         iv: [u8; 16],
         plaintext: &[u8],
-        cipher: &mut [u8],
+        ciphertext: &mut [u8],
     ) -> Result<(), Self::Error>;
 
-    fn impl_decrypt(
-        &mut self,
-        key: [u8; 16],
-        iv: [u8; 16],
-        cipher: &[u8; 16],
-        plaintext: &mut [u8; 16],
-    ) -> Result<(), Self::Error>;
     fn decrypt(
         &mut self,
         key: [u8; 16],
         iv: [u8; 16],
-        cipher: &[u8],
+        ciphertext: &[u8],
         plaintext: &mut [u8],
     ) -> Result<(), Self::Error>;
 
@@ -55,84 +40,84 @@ pub trait AES128Cbc {
     // Notify user if SW-impl is used
 }
 
-pub struct s32k144AES {
+pub struct S32k144AES {
     csec: csec::CSEc,
 }
 
-impl s32k144AES {
+impl S32k144AES {
     pub fn new(ftfc: s32k144::FTFC, cse_pram: s32k144::CSE_PRAM) -> Self {
-        s32k144AES {
+        S32k144AES {
             csec: csec::CSEc::init(ftfc, cse_pram),
         }
     }
-}
-
-impl AES128Cbc for s32k144AES {
-    type Error = Error;
 
     fn impl_encrypt(
         &mut self,
         key: [u8; 16],
         iv: [u8; 16],
         plaintext: &[u8; 16],
-        cipher: &mut [u8; 16],
+        ciphertext: &mut [u8; 16],
     ) -> Result<(), Error> {
         self.csec.load_plainkey(&key)?;
-        self.csec.encrypt_cbc(plaintext, &iv, cipher)
-    }
-
-    fn encrypt(
-        &mut self,
-        key: [u8; 16],
-        iv: [u8; 16],
-        plaintext: &[u8],
-        cipher: &mut [u8],
-    ) -> Result<(), Error> {
-        // if N % 16 != 0 {
-        //     return Err(Error::GeneralError);
-        // }
-
-        // NOTE(unwrap): With the check above, each chunk will always have the length 16.
-        for (plaintext_chunk, cipher_chunk) in plaintext.chunks(16).zip(cipher.chunks_mut(16)) {
-            self.impl_encrypt(
-                key,
-                iv,
-                plaintext_chunk.try_into().unwrap(),
-                cipher_chunk.try_into().unwrap(),
-            )?;
-        }
-        Ok(())
+        self.csec.encrypt_cbc(plaintext, &iv, ciphertext)
     }
 
     fn impl_decrypt(
         &mut self,
         key: [u8; 16],
         iv: [u8; 16],
-        cipher: &[u8; 16],
+        ciphertext: &[u8; 16],
         plaintext: &mut [u8; 16],
     ) -> Result<(), Error> {
         self.csec.load_plainkey(&key)?;
-        self.csec.decrypt_cbc(cipher, &iv, plaintext)
+        self.csec.decrypt_cbc(ciphertext, &iv, plaintext)
+    }
+}
+
+impl AES128Cbc for S32k144AES {
+    type Error = Error;
+
+    fn encrypt(
+        &mut self,
+        key: [u8; 16],
+        iv: [u8; 16],
+        plaintext: &[u8],
+        ciphertext: &mut [u8],
+    ) -> Result<(), Error> {
+        if plaintext.len() % 16 != 0 && plaintext.len() != ciphertext.len() {
+            return Err(Error::GeneralError);
+        }
+
+        // NOTE(unwrap): With the check above, each chunk will always have the length 16.
+        for (plain_chunk, cipher_chunk) in plaintext.chunks(16).zip(ciphertext.chunks_mut(16)) {
+            self.impl_encrypt(
+                key,
+                iv,
+                plain_chunk.try_into().unwrap(),
+                cipher_chunk.try_into().unwrap(),
+            )?;
+        }
+        Ok(())
     }
 
     fn decrypt(
         &mut self,
         key: [u8; 16],
         iv: [u8; 16],
-        cipher: &[u8],
+        ciphertext: &[u8],
         plaintext: &mut [u8],
     ) -> Result<(), Error> {
-        // if N % 16 != 0 {
-        //     return Err(Error::GeneralError);
-        // }
+        if plaintext.len() % 16 != 0 && plaintext.len() != ciphertext.len() {
+            return Err(Error::GeneralError);
+        }
 
         // NOTE(unwrap): With the check above, each chunk will always have the length 16.
-        for (plaintext_chunk, cipher_chunk) in plaintext.chunks_mut(16).zip(cipher.chunks(16)) {
+        for (plain_chunk, cipher_chunk) in plaintext.chunks_mut(16).zip(ciphertext.chunks(16)) {
             self.impl_decrypt(
                 key,
                 iv,
                 cipher_chunk.try_into().unwrap(),
-                plaintext_chunk.try_into().unwrap(),
+                plain_chunk.try_into().unwrap(),
             )?;
         }
         Ok(())
