0f7508a80b92c2b66821a877df648775cac87f66
tmplt <tmplt@dragons.rocks>
2020-02-27 17:56:40 +0100

    jlink: verify program flashing

--- gdbserver.sh
+++ gdbserver.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-JLinkGDBServerCLExe -if swd -device S32K144 -endian little -speed 1000 -port 2331 -swoport 2332 -telnetport 2333 -vd -ir -localhostonly 1 -strict -timeout 0 -nogui
+JLinkGDBServerCLExe -if swd -device S32K144 -endian little -speed 1000 -port 2331 -swoport 2332 -telnetport 2333 -vd -ir -localhostonly 1 -strict -timeout 0 -nogui -vd
